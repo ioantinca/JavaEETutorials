c9fb10f0654ebb1c8646d5dd8e5f7d91775cca86
ioan.tinca <ioan.tinca@192.168.16.19>
2014-12-03 18:05:43 +0200

    test

--- JavaApplication/src/javaapplication/JavaApplication.java
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -15,6 +15,7 @@ public class JavaApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //to do
         // TODO code application logic here
     }
     
